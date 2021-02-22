b58651e1b7dd732b4f25f2a7d51650fb30afb7da
dummyvariable <officialsanjivee@gmail.com>
2021-02-23 00:34:35 +0530

    added after-request processor for disabling browser cache

--- front-end/frontend.py
+++ front-end/frontend.py
@@ -3,4 +3,10 @@ from config import Config,TestConfig
 
 app = create_app('prod')
 
+@app.after_request
+def add_header(response):
+    response.headers['Cache-Control'] = 'public, max-age=0'
+    return response
+    
+
 app.run(port=8080)
