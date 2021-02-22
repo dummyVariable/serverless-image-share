a01cce8ddd805e3a828770eaf329f6b7ed852522
dummyvariable <officialsanjivee@gmail.com>
2021-02-23 00:35:43 +0530

    updated texarefield to filefield

--- front-end/app/main/forms.py
+++ front-end/app/main/forms.py
@@ -1,9 +1,9 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextAreaField, SubmitField
+from wtforms import StringField, FileField, SubmitField
 from wtforms.validators import DataRequired
 
 class UploadForm(FlaskForm):
 
     title = StringField('Title', validators=[DataRequired()])
-    image = TextAreaField('Image', validators=[DataRequired()])
+    image = FileField('Image', validators=[DataRequired()])
     sumbit = SubmitField('Upload')
