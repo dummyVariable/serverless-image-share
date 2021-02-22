0460412ee74845b4fc1b87fe477ddd5b58d90c41
dummyvariable <officialsanjivee@gmail.com>
2021-02-22 20:48:58 +0530

    removed unwanted content

--- front-end/app/__init__.py
+++ front-end/app/__init__.py
@@ -1,21 +1,14 @@
 from flask import Flask
-from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
-from flask_login import LoginManager
 
 from config import option
 
-login_manager = LoginManager()
-login_manager.login_view = 'auth.login'
 bootstrap = Bootstrap()
-db = SQLAlchemy()
 
 def create_app(env):
     
     app = Flask(__name__)
     app.config.from_object(option[env])
-
-    db.init_app(app)
 
     from app.main import main as main_blueprint
     app.register_blueprint(main_blueprint)
@@ -23,7 +16,7 @@ def create_app(env):
     from app.auth import auth as auth_blueprint
     app.register_blueprint(auth_blueprint)
 
-    login_manager.init_app(app)
+
     bootstrap.init_app(app)
 
 
--- front-end/config.py
+++ front-end/config.py
@@ -1,11 +1,8 @@
 import os
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
 
-    SQLALCHEMY_DATABASE_URI = f'sqlite:///{BASEDIR}/data.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'secret'
     BOOTSTRAP_USE_MINIFIED = True
 
