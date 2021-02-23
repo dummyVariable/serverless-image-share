031c871104d3332069766fef774836e4b1549834
dummyvariable <officialsanjivee@gmail.com>
2021-02-23 20:40:29 +0530

    added context-processor to add login variable that will be used by templates

--- front-end/frontend.py
+++ front-end/frontend.py
@@ -1,8 +1,13 @@
 from app import create_app
 from config import Config,TestConfig
 
+from app.auth.model import is_logged
+
 app = create_app('prod')
 
+@app.context_processor
+def check_if_logged_in():
+    return dict(login=is_logged())
 @app.after_request
 def add_header(response):
     response.headers['Cache-Control'] = 'public, max-age=0'
