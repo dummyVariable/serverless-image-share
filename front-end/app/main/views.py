052676174fb55d346e7e054491ac0903fc765b91
dummyvariable <officialsanjivee@gmail.com>
2021-02-23 20:27:31 +0530

    updated upload view that only logged in users can upload

--- front-end/app/main/views.py
+++ front-end/app/main/views.py
@@ -1,9 +1,11 @@
 
-from flask import render_template, redirect, url_for, request
+from flask import render_template, redirect, url_for, request, flash
 
 from app.main import main
 from app.main.forms import UploadForm
 from app.main.model import save_to_S3, get_all_images, get_image_by_id, get_image_by_tag
+
+from app.auth.model import is_logged
 '''
 image = {
     'id',
@@ -32,9 +34,15 @@ def image(id: int):
 def upload():
     form = UploadForm()
     if form.validate_on_submit():
+        if not is_logged:
+            flash('Login to upload')
+            return redirect(url_for('auth.login'))
         filename = form.title.data
         image = form.image.data.read()
-        save_to_S3(filename, image)
+        done = save_to_S3(filename, image)
+        if not done:
+            flash('Something went wrong, Try again')
+            return redirect(url_for('main.upload'))
         return redirect(url_for('main.index'))
     return render_template('upload.html', form=form)
 
