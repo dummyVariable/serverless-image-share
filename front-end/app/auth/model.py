ee8666ef20e5dc15446621846c09a906a067a017
dummyvariable <officialsanjivee@gmail.com>
2021-02-23 20:02:43 +0530

    Revert "added get_user function to fetch username"
    This reverts commit 8d79eef807bd57e267d1b32d759bc1afa7ff6582.

--- front-end/app/auth/model.py
+++ front-end/app/auth/model.py
@@ -58,23 +58,6 @@ def confirm_sign_up_validation(username: str, code: str):
     
     return True
 
-def get_user(token: str):
-    
-    try:
-        resp = requests.post(f'{endpoint}/get_user', 
-                            data={
-                                token : token
-                            })
-    
-    except Exception as e:
-        print(e)
-        return None, True
-    
-    if resp['error'] is False:
-        return resp['user'], False
-    
-    return None, True
-
 def set_cookie(token: str):
     resp = make_response(redirect(url_for('main.index')))
     resp.set_cookie('token','value', samesite=None)
