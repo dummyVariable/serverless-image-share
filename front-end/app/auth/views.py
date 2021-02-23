fc2c5fb1cf67095d24fe62bd0f262b196199de66
dummyvariable <officialsanjivee@gmail.com>
2021-02-23 19:44:04 +0530

    updated sign_up view to use sign_up_calidation on submit

--- front-end/app/auth/views.py
+++ front-end/app/auth/views.py
@@ -2,7 +2,7 @@ from flask import render_template, redirect, flash, url_for
 
 from app.auth import auth
 from app.auth.forms import LoginForm, SignUpForm, ConfirmSignUpForm
-from app.auth.model import login_validation, set_cookie
+from app.auth.model import login_validation, sign_up_validation, set_cookie
 
 
 @auth.route('/login', methods=['POST','GET'])
@@ -25,6 +25,15 @@ def sign_up():
     form = SignUpForm()
     
     if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        email = form.email.data
+
+        error = sign_up_validation(username, password, email)
+
+        if error:
+            return redirect(url_for('auth.sign_up'))
+
         return redirect(url_for('auth.confirm_sign_up'))
 
     return render_template('/auth/sign-up.html', form=form)
